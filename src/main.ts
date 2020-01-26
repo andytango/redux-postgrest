fd01837e7d33ca4d0a7858f8b8cf337f31474637
Andrew Hall <andrew@rioja.io>
2020-01-26 11:58:32 +0000

    Logic to queue actions pending endpoint response

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,44 @@
 import "./config"
+import { Store, Action, Middleware, Dispatch } from "redux"
+import axios from "axios"
 
-export default function main() {
-  return true
+export interface HttpClient {
+  get: (url: String) => Promise<Object>
+}
+
+export interface PostgrestOpts {
+  http: HttpClient
+  url: URL
+}
+
+const queuedActions: Array<Action> = []
+
+export default function connectPostgrest(opts: PostgrestOpts) {
+  const { http = axios, url } = opts
+
+  const apiRoot = {
+    loaded: false,
+    body: {},
+  }
+
+  return (store: Store) => {
+    http.get(url.toString()).then(body => {
+      apiRoot.body = body
+      apiRoot.loaded = true
+
+      while (queuedActions.length) {
+        store.dispatch(queuedActions.pop())
+      }
+    })
+
+    return (next: Dispatch) => (action: Action) => {
+      if (apiRoot.loaded) {
+        return next(action)
+      }
+
+      queuedActions.push(action)
+
+      return store.getState()
+    }
+  }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,105 @@
-import main from "../src/main"
+import connectPostgrest, { HttpClient } from "../src/main"
+import {
+  createStore,
+  applyMiddleware,
+  Action,
+} from "redux"
 
-describe("main", () => {
-  it("returns true", () => {
-    expect(main()).toBeTruthy()
+describe("connectPostgrest", () => {
+  it("calls GET on the http client with the correct uri", () => {
+    const mockHttp: HttpClient = {
+      get: jest.fn((url: String) => Promise.resolve({})),
+    }
+
+    createStore(
+      (state, action) => {},
+      applyMiddleware(
+        connectPostgrest({
+          http: mockHttp,
+          url: new URL("http://example.com/"),
+        }),
+      ),
+    )
+
+    expect(mockHttp.get).toHaveBeenCalledWith("http://example.com/")
+  })
+
+  it("does not pass actions until the request has completed", done => {
+    const mockHttp: HttpClient = {
+      get: jest.fn(
+        (url: String) =>
+          new Promise(res => {
+            setImmediate(() => {
+              expect(exampleReducer).toHaveBeenNthCalledWith(
+                1,
+                undefined,
+                expect.any(Object),
+              )
+              expect(exampleReducer).toHaveBeenCalledTimes(1)
+              res({})
+              done()
+            })
+          }),
+      ),
+    }
+
+    const exampleState = {}
+    const exampleReducer = jest.fn(() => exampleState)
+    const exampleAction: Action = { type: "EXAMPLE_TYPE" }
+
+    const store = createStore(
+      exampleReducer,
+      applyMiddleware(
+        connectPostgrest({
+          http: mockHttp,
+          url: new URL("http://example.com/"),
+        }),
+      ),
+    )
+
+    store.dispatch(exampleAction)
+  })
+
+  it("does not pass actions until the request has completed", done => {
+    const mockHttp: HttpClient = {
+      get: jest.fn(
+        (url: String) =>
+          new Promise(res => {
+            setImmediate(() => {
+              res({})
+
+              setImmediate(() => {
+                expect(exampleReducer).toHaveBeenNthCalledWith(
+                  1,
+                  undefined,
+                  expect.any(Object),
+                )
+                expect(exampleReducer).toHaveBeenNthCalledWith(
+                  2,
+                  exampleState,
+                  exampleAction,
+                )
+                done()
+              })
+            })
+          }),
+      ),
+    }
+
+    const exampleState = {}
+    const exampleReducer = jest.fn(() => exampleState)
+    const exampleAction: Action = { type: "EXAMPLE_TYPE" }
+
+    const store = createStore(
+      exampleReducer,
+      applyMiddleware(
+        connectPostgrest({
+          http: mockHttp,
+          url: new URL("http://example.com/"),
+        }),
+      ),
+    )
+
+    store.dispatch(exampleAction)
   })
 })
