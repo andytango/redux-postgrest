2f24a22fd0674667747d549af002d3bcc2441063
Andrew Hall <andrew@rioja.io>
2020-02-11 17:28:00 +0000

    Add RPC example to db test prep

--- scripts/db_test_prep.sh
+++ scripts/db_test_prep.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 psql postgres://postgres:secret@localhost:5432/postgres -c '
-CREATE TABLE public.example_table (
+CREATE TABLE IF NOT EXISTS public.example_table (
     example_id INTEGER GENERATED ALWAYS AS IDENTITY
         PRIMARY KEY,
     example_text TEXT,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
+)
+' -c '
+CREATE OR REPLACE FUNCTION public.example_fn(example_param TEXT)
+RETURNS TEXT
+LANGUAGE SQL
+AS $sql$
+SELECT $$some_text$$
+$sql$
 '
