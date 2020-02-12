f02e4d8530d8d5402eb056bf2da4d0206ffb9547
Andrew Hall <andrew@rioja.io>
2020-02-12 10:00:12 +0000

    Extend interfaces for new actionHttp handler

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -1,3 +1,10 @@
+export interface HttpResponse {
+  data: Object
+}
+
 export interface HttpClient {
-  get: (url: String) => Promise<Object>
+  get: (url: String) => Promise<HttpResponse>
+  post: (url: String) => Promise<HttpResponse>
+  patch: (url: String) => Promise<HttpResponse>
+  delete: (url: String) => Promise<HttpResponse>
 }
--- src/PostgrestAction.ts
+++ src/PostgrestAction.ts
@@ -1,9 +1,11 @@
 import { Action } from "redux"
 import { HttpMethod } from "./HttpMethod"
 import { ActionKind } from "./ActionKind"
+
 export interface PostgrestAction extends Action {
   meta: {
     method: HttpMethod
+    url: string
     kind: ActionKind
   }
 }
