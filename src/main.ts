9968949379ea57fbede3beba7bbd42af349d5e92
Andrew Hall <andrew@rioja.io>
2020-02-12 09:59:34 +0000

    Clear main.js and tests pending new structure

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,22 @@
+import { Action, Dispatch, Middleware, Store } from "redux"
 import "./config"
-import { Store, Action, Middleware, Dispatch } from "redux"
-import axios from "axios"
-import { ApiRoot, isPostgrestAction } from "./lib"
-import { HttpMethod } from "./HttpMethod"
-import { ActionKind } from "./ActionKind"
-import { PostgrestAction } from "./PostgrestAction"
 import { HttpClient } from "./HttpClient"
 
 export interface PostgrestOpts {
-  http: HttpClient
-  url: URL
+  http?: HttpClient
+  url: string
 }
 
-const queuedActions: Array<Action> = []
+export interface PostgrestOptsInternal {
+  http?: HttpClient
+  url: string
+}
 
 export default function connectPostgrest(opts: PostgrestOpts): Middleware {
-  const { http = axios, url } = opts
-
-  const apiRoot: ApiRoot = {
-    loaded: false,
-    body: {},
-  }
-
   return (store: Store) => {
-    http.get(url.toString()).then(body => {
-      apiRoot.body = body
-      apiRoot.loaded = true
-
-      while (queuedActions.length) {
-        store.dispatch(queuedActions.pop())
-      }
-    })
-
     return (next: Dispatch) => (action: Action) => {
-      if (apiRoot.loaded) {
-        return next(addMeta(action, apiRoot))
-      }
-
-      queuedActions.push(action)
-
+      next(action)
       return store.getState()
     }
   }
-}
-
-function addMeta(action: Action, apiRoot: ApiRoot): PostgrestAction | Action {
-  if (isPostgrestAction(action, apiRoot)) {
-    return {
-      ...action,
-      meta: {
-        method: HttpMethod.GET,
-        kind: ActionKind.REQUEST,
-      },
-    }
-  }
-
-  return action
 }
