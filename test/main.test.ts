665f58ef91928e5e7fcb794dc9cc66a308f09674
Andrew Hall <andrew@rioja.io>
2020-02-22 16:08:01 +0000

    Initial implementation and test for middleware

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,67 @@
-import Axios from "axios"
+import Axios, { AxiosRequestConfig, AxiosResponse } from "axios"
 import connectPostgrest from "../src/main"
+import { createStore, Action, applyMiddleware } from "redux"
+
 describe("connectPostgrest", () => {
   it("returns a middleware", () => {
     expect(
       connectPostgrest({ http: Axios, url: "http://localhost:3000" }),
     ).toBeInstanceOf(Function)
   })
+
+  it.skip("passes actions", () => {
+    const middleware = connectPostgrest({
+      http: Axios,
+      url: "http://localhost:3000",
+    })
+
+    const reducer = (state = { lastAction: {} }, action: Action) => ({
+      lastAction: action,
+    })
+
+    const store = createStore(reducer, applyMiddleware(middleware))
+
+    store.dispatch({ type: "example_action" })
+
+    expect(store.getState().lastAction).toEqual({ type: "example_action" })
+  })
+
+  it.skip("adds action metadata", done => {
+    const http = wrapAxios(() => {
+      expect(store.getState().lastAction).toMatchInlineSnapshot(`
+      Object {
+        "type": "example_table",
+      }
+    `)
+      done()
+    })
+
+    const middleware = connectPostgrest({
+      http,
+      url: "http://localhost:3000",
+    })
+
+    const reducer = (state = { lastAction: {} }, action: Action) => ({
+      lastAction: action,
+    })
+
+    const store = createStore(reducer, applyMiddleware(middleware))
+
+    store.dispatch({ type: "example_table" })
+  })
 })
+
+function wrapAxios(fn) {
+  const invoker = method => (url: string) =>
+    Axios[method](url).then((res: AxiosResponse) => {
+      setImmediate(() => fn(res))
+      return res
+    })
+
+  return {
+    get: invoker("get"),
+    post: invoker("post"),
+    patch: invoker("patch"),
+    delete: invoker("delete"),
+  }
+}
