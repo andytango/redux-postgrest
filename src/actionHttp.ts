b64fb6e953d6f33f902c9d264be7d74d3c0a1cf5
Andrew Hall <andrew@rioja.io>
2020-02-22 17:54:16 +0000

    Slight refactor

--- src/actionHttp.ts
+++ src/actionHttp.ts
@@ -2,22 +2,27 @@ import { Action, Store } from "redux"
 import { PostgrestAction } from "./PostgrestAction"
 import { pathEq, pipe, path, toLower } from "ramda"
 import { PostgrestOpts } from "./main"
-import { HttpResponse, HttpKind } from "./http"
+import { HttpResponse, HttpKind, HttpClient } from "./http"
 import logger from "./log"
 
 export default function actionHttp(opts: PostgrestOpts, store: Store) {
   logger.verbose("Action HTTP handler initialised")
-  
+
   return (action: Action) => {
     if (isHttpRequestAction(action)) {
-      logRequestAction(action)
-      opts.http[httpClientMethod(action)](action.meta.url).then(
-        onResponse(action, store),
-      )
+      logRequest(action)
+      performHttpRequest(opts.http, action).then((response: HttpResponse) => {
+        logResponse(action)
+        dispatchResponse(store, action, response)
+      })
     }
 
     return action
   }
+}
+
+function performHttpRequest(http: HttpClient, action: PostgrestAction) {
+  return http[httpClientMethod(action)](action.meta.url)
 }
 
 const isHttpRequestAction = <(action: Action) => action is PostgrestAction>(
@@ -29,21 +34,22 @@ const httpClientMethod: (action: PostgrestAction) => string = pipe(
   toLower,
 )
 
-function onResponse(action: PostgrestAction, store: Store) {
-  return (response: HttpResponse) => {
-    logResponseAction(action)
-    store.dispatch({
-      ...action,
-      meta: {
-        ...action.meta,
-        kind: HttpKind.RESPONSE,
-        response,
-      },
-    })
-  }
+function dispatchResponse(
+  store: Store,
+  action: PostgrestAction,
+  response: HttpResponse,
+) {
+  store.dispatch({
+    ...action,
+    meta: {
+      ...action.meta,
+      kind: HttpKind.RESPONSE,
+      response,
+    },
+  })
 }
 
-function logRequestAction(action: PostgrestAction) {
+function logRequest(action: PostgrestAction) {
   logger.info(
     `HTTP request action received for type ${
       action.type
@@ -51,7 +57,7 @@ function logRequestAction(action: PostgrestAction) {
   )
 }
 
-function logResponseAction(action: PostgrestAction) {
+function logResponse(action: PostgrestAction) {
   logger.info(
     `HTTP response action received for type ${
       action.type
