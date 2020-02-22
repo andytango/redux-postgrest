61c76b7fce93119fccd1246c5dd3a6aeaf04003f
Andrew Hall <andrew@rioja.io>
2020-02-22 18:07:33 +0000

    Record http requests during testing

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   preset: "ts-jest",
-  setupFiles: ["dotenv/config"],
+  setupFiles: ["dotenv/config", "sepia/index"],
   testEnvironment: "node",
   testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 }
