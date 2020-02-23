711ff894f02fe16b234d4cf9546cdc10d9592e95
Andrew Hall <andrew@rioja.io>
2020-02-23 16:02:08 +0000

    Switch to yarn, update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import pkg from "./package.json"
-import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
 import ts from "@wessberg/rollup-plugin-ts"
+import pkg from "./package.json"
 
 export default [
   // browser-friendly UMD build
   {
     input: "src/main.ts",
-    external: ["fs", "path", "dotenv"],
+    external: ["fs", "path", "dotenv", "winston"],
     output: {
       name: "main",
       file: pkg.browser,
@@ -16,15 +16,9 @@ export default [
     },
     plugins: [resolve(), commonjs(), ts()],
   },
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: "src/main.ts",
-    external: ["fs", "path", "dotenv"],
+    external: ["fs", "path", "dotenv", "winston"],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
