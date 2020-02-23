28355fd898e98a13b3e2967d18400de16229f485
Andrew Hall <andrew@rioja.io>
2020-02-23 17:05:50 +0000

    Remove umd from buid

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,10 @@
-import commonjs from "@rollup/plugin-commonjs"
-import resolve from "@rollup/plugin-node-resolve"
 import ts from "@wessberg/rollup-plugin-ts"
 import pkg from "./package.json"
 
 export default [
-  // browser-friendly UMD build
   {
     input: "src/main.ts",
-    external: ["fs", "path", "dotenv", "winston"],
-    output: {
-      name: "main",
-      file: pkg.browser,
-      format: "umd",
-      globals: { dotenv: "dotenv" },
-    },
-    plugins: [resolve(), commonjs(), ts()],
-  },
-  {
-    input: "src/main.ts",
-    external: ["fs", "path", "dotenv", "winston"],
+    external: ["fs", "path", "dotenv"],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
