ae91fa2578475e4fe557eda278774adc8a9c541d
Andrew Hall <andrew@rioja.io>
2020-02-26 18:42:10 +0000

    Fix fetch adapter
    Add json request header
    Handle empty responses

--- src/httpFetch.ts
+++ src/httpFetch.ts
@@ -2,17 +2,24 @@ import { HttpRequestConfig, HttpResponse } from "./http"
 import "isomorphic-fetch"
 
 export function httpFetch(config: HttpRequestConfig): Promise<HttpResponse> {
-  const { url, method, body } = config
+  const { url, method, body, headers } = config
   return fetch(url, {
     method,
     body: body ? JSON.stringify(body) : null,
+    headers: {
+      "content-type": "application/json",
+      ...headers,
+    },
   }).then(res =>
-    res.json().then(body => ({
-      body,
-      status: res.status,
-      statusText: res.statusText,
-      headers: res.headers,
-      url,
-    })),
+    res
+      .json()
+      .catch(() => undefined)
+      .then(body => ({
+        body,
+        status: res.status,
+        statusText: res.statusText,
+        headers: res.headers,
+        url,
+      })),
   )
 }
