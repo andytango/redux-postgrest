3162398d7787d0176a54546cfad0a015d372d3e0
Andrew Hall <andrew@rioja.io>
2020-02-27 13:41:24 +0000

    Update rollup config and packages

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,16 @@ import pkg from "./package.json"
 export default [
   {
     input: "src/main.ts",
-    external: ["fs", "path", "dotenv", "ramda", "isomorphic-fetch"],
+    external: [
+      "fs",
+      "path",
+      "dotenv",
+      "ramda",
+      "redux",
+      "isomorphic-fetch",
+      "winston",
+      "query-string",
+    ],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
